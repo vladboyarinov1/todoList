f311466e28e35160190bccd8b7e4ac77ea6ff3cc
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-08-02 18:16:21 +0300

    refactor: add changes

--- src/index.tsx
+++ src/index.tsx
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 reportWebVitals();
